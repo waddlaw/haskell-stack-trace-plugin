7a8a9ed021286ae02f55bba2f8e4a8d3c690370a
James Blackburn <blackburnfjames@gmail.com>
2025-01-03 08:34:40 +0000

    Add testing support on ghc >= 9.4 (#21)
    * build: move from deprecated gh action, invalidate cache
    
    * build: move to newer, more performant cabal
    
    * build: run gh-actions on ghc 9.12
    
    * build: add support for newer dependencies when testing
    
    * fix: tests on ghc >= 9.4
    
    * build: remove support for ghc ^>= 9.0 and 9.2
    
    * build: no longer test on unsupported ghc 9.0 and 9.2
    
    * build: use recommended caching system
    
    * build: use alternative caching mechanism
    
    * chore: remove unused test comparison

--- test/StackTrace/PluginSpec.hs
+++ test/StackTrace/PluginSpec.hs
@@ -1,17 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module StackTrace.PluginSpec (spec) where
 
 import Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy as BL
-import Test.Hspec
+import qualified Data.ByteString.Lazy.Char8 as BS
 import System.Process.Typed
+import Test.Hspec
+import Text.RawString.QQ
+import Text.Regex.TDFA ((=~))
+
+-- | Extracts the function names of all functions that appear in a stack trace
+extractStackTraceFunctions :: String -> [String]
+extractStackTraceFunctions input = map extractFunctionName matches
+ where
+  regex = [r|[ ]*([A-Za-z0-9]+), called at example/Main.hs:[0-9]+:[0-9]+ in [^:]+:Main|] :: String
+  matches = (input =~ regex) :: [[String]]
+  extractFunctionName :: [String] -> String
+  extractFunctionName (_ : name : _) = name
+  extractFunctionName _ = error "Regex match failed"
 
 spec :: Spec
 spec = do
   output <- runIO exe
-  expected <- runIO $ BL.readFile "test/resource/ghc.output"
-  it "integration test" $ output `shouldBe` expected
+  let outputStr = BS.unpack output
+  let extractedFunctions = extractStackTraceFunctions outputStr
 
+  -- each of these exceptions should appear in the stack trace
+  let expectedFunctions = ["error", "fError", "f10", "f9", "f8", "f7", "f6", "f5", "f4", "f3", "f2", "f1", "main"] :: [String]
+
+  it "integration test" $ extractedFunctions `shouldBe` expectedFunctions
 
 exe :: IO ByteString
 exe = do
